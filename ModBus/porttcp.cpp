22e23fd82c4fede5d9e6c7f04d3420148f3df8e3
paleskyjp <undefined>
2012-03-13 09:11:49 +0000

    The first modification of ModbusTCP

---
+++ ModBus/porttcp.cpp
@@ -0,0 +1,312 @@
+/*
+ * FreeModbus Libary: mbed Port
+ * Copyright (C) 2006 Christian Walter <wolti@sil.at>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * File: $Id: porttcp.c,v 1.1 2006/08/30 23:18:07 wolti Exp $
+ */
+/* ----------------------- System includes ----------------------------------*/
+#include <stdio.h>
+#include <string.h>
+
+#include "port.h"
+
+/* ----------------------- mbed includes ------------------------------------*/
+#include "mbed.h"
+#include "EthernetNetIf.h"
+#include "TCPSocket.h"
+
+/* ----------------------- Modbus includes ----------------------------------*/
+#include "mb.h"
+#include "mbport.h"
+
+/* ----------------------- MBAP Header --------------------------------------*/
+#define MB_TCP_UID          6
+#define MB_TCP_LEN          4
+#define MB_TCP_FUNC         7
+
+/* ----------------------- Defines  -----------------------------------------*/
+#define MB_TCP_DEFAULT_PORT 502 /* TCP listening port. */
+#define MB_TCP_BUF_SIZE     ( 256 + 7 ) /* Must hold a complete Modbus TCP frame. */
+
+/* ----------------------- Prototypes ---------------------------------------*/
+void prvvMBPortReleaseClient(  );
+BOOL prvbMBPortAcceptClient(  );
+BOOL prvMBTCPGetFrame(  );
+void onListeningTCPSocketEvent(TCPSocketEvent e);
+void onConnectedTCPSocketEvent(TCPSocketEvent e);
+
+/* ----------------------- Static variables ---------------------------------*/
+static UCHAR    aucTCPBuf[MB_TCP_BUF_SIZE];
+static USHORT   usTCPBufPos;
+static USHORT   usTCPFrameBytesLeft;
+
+static TCPSocket ListeningSock;
+static TCPSocket* pConnectedSock; // for ConnectedSock
+static Host client;
+
+/* ----------------------- Begin implementation -----------------------------*/
+BOOL
+xMBTCPPortInit( USHORT usTCPPort )
+{
+    BOOL            bOkay = FALSE;
+    USHORT          usPort;
+
+    if( usTCPPort == 0 )
+    {
+        usPort = MB_TCP_DEFAULT_PORT;
+    }
+    else
+    {
+        usPort = ( USHORT ) usTCPPort;
+    }
+    pConnectedSock=NULL;
+
+    // Set the callbacks for Listening
+    ListeningSock.setOnEvent(&onListeningTCPSocketEvent); 
+
+    // bind and listen on TCP
+    if( ListeningSock.bind(Host(IpAddr(), usPort)) )
+    {
+        // Failed to bind
+        bOkay = FALSE;
+    }
+    else if( ListeningSock.listen() )
+    {
+        // Failed to listen
+        bOkay = FALSE;
+    }
+    else
+    {
+#ifdef MB_TCP_DEBUG
+        vMBPortLog( MB_LOG_DEBUG, "MBTCP-ACCEPT", "Protocol stack ready.\r\n" );
+#endif
+        bOkay = TRUE;
+    }    
+
+    return bOkay;
+}
+
+void
+vMBTCPPortClose(  )
+{
+    /* Shutdown any open client sockets. */
+    prvvMBPortReleaseClient();
+
+    /* Shutdown or listening socket. */
+    ListeningSock.close();
+
+    /* Release resources for the event queue. */
+//    vMBPortEventClose(  );
+}
+
+void
+vMBTCPPortDisable( void )
+{
+    prvvMBPortReleaseClient( );
+}
+
+BOOL
+xMBTCPPortGetRequest( UCHAR ** ppucMBTCPFrame, USHORT * usTCPLength )
+{
+    *ppucMBTCPFrame = &aucTCPBuf[0];
+    *usTCPLength = usTCPBufPos;
+
+    /* Reset the buffer. */
+    usTCPBufPos = 0;
+    usTCPFrameBytesLeft = MB_TCP_FUNC;
+    return TRUE;
+}
+
+BOOL
+xMBTCPPortSendResponse( const UCHAR * pucMBTCPFrame, USHORT usTCPLength )
+{
+    BOOL            bFrameSent = FALSE;
+
+    if( pConnectedSock )
+    {
+        if(pConnectedSock->send((char *)pucMBTCPFrame, usTCPLength)>=0)
+        {
+            bFrameSent = TRUE;
+            printf("sent %d bytes\n",usTCPLength);
+        }
+        else
+        {
+            /* Drop the connection in case of an write error. */
+            printf("sent error!\n");
+            prvvMBPortReleaseClient( );
+        }
+    }
+    return bFrameSent;
+}
+
+void
+prvvMBPortReleaseClient(  )
+{
+    if(pConnectedSock){
+        IpAddr clientIp = client.getIp();
+#ifdef MB_TCP_DEBUG
+        vMBPortLog( MB_LOG_DEBUG, "MBTCP-CLOSE", "Closed connection to %d.%d.%d.%d.\r\n",
+             clientIp[0], clientIp[1], clientIp[2], clientIp[3]);
+#endif
+        pConnectedSock->close();
+        pConnectedSock=NULL;
+    }
+}
+
+
+BOOL prvbMBPortAcceptClient(  )
+{
+    // Accepts connection from client and gets connected socket.   
+
+    if (ListeningSock.accept(&client, &pConnectedSock)) {
+        return FALSE; //Error in accept, discard connection
+    }
+    // Setup the new socket events
+    pConnectedSock->setOnEvent(&onConnectedTCPSocketEvent);
+    // We can find out from where the connection is coming by looking at the
+    // Host parameter of the accept() method
+    IpAddr clientIp = client.getIp();
+
+#ifdef MB_TCP_DEBUG
+    vMBPortLog( MB_LOG_DEBUG, "MBTCP-ACCEPT", "Accepted new client %d.%d.%d.%d\r\n",
+        clientIp[0], clientIp[1], clientIp[2], clientIp[3]);
+#endif
+
+    usTCPBufPos = 0;
+    usTCPFrameBytesLeft = MB_TCP_FUNC;
+
+    return TRUE;
+}
+
+
+BOOL
+prvMBTCPGetFrame(  )
+{
+    BOOL            bOkay = TRUE;
+    USHORT          usLength;
+    int             iRes;
+    int i;
+    int total=0;
+    int len;
+    char buf[MB_TCP_BUF_SIZE];
+    /* Make sure that we can safely process the next read request. If there
+     * is an overflow drop the client.
+     */
+    
+    if( ( usTCPBufPos + usTCPFrameBytesLeft ) >= MB_TCP_BUF_SIZE )
+    {
+        // buffer overrun
+        return FALSE;
+    }
+
+    /* Though this part is verbose, it doesn't work when data is recieved directly into aucTCPBuf. */
+    while (len = pConnectedSock->recv(&buf[total], MB_TCP_BUF_SIZE-total) ) {
+        total+=len;
+    }
+
+    if(total>0){
+        memcpy((char *)&aucTCPBuf[usTCPBufPos],buf,total);
+        usTCPBufPos+=total;
+        usTCPFrameBytesLeft-=total;
+        prvMBTCPGetFrame();
+    }
+
+    /* If we have received the MBAP header we can analyze it and calculate
+     * the number of bytes left to complete the current request. If complete
+     * notify the protocol stack.
+     */
+    if( usTCPBufPos >= MB_TCP_FUNC )
+    {
+        /* Length is a byte count of Modbus PDU (function code + data) and the
+         * unit identifier. */
+        usLength = aucTCPBuf[MB_TCP_LEN] << 8U;
+        usLength |= aucTCPBuf[MB_TCP_LEN + 1];
+
+        /* Is the frame already complete. */
+        if( usTCPBufPos < ( MB_TCP_UID + usLength ) )
+        {
+            usTCPFrameBytesLeft = usLength + MB_TCP_UID - usTCPBufPos;
+        }
+        /* The frame is complete. */
+        else if( usTCPBufPos == ( MB_TCP_UID + usLength ) )
+        {
+#ifdef MB_TCP_DEBUG
+            prvvMBTCPLogFrame( "MBTCP-RECV", &aucTCPBuf[0], usTCPBufPos );
+#endif
+            ( void )xMBPortEventPost( EV_FRAME_RECEIVED );
+        }
+        else
+        {
+#ifdef MB_TCP_DEBUG
+            vMBPortLog( MB_LOG_DEBUG, "MBTCP-ERROR",
+                            "Received too many bytes! Droping client.[%d>%d]\r\n" ,usTCPBufPos, MB_TCP_UID + usLength);
+#endif
+            /* This should not happen. We can't deal with such a client and
+             * drop the connection for security reasons.
+             */
+            prvvMBPortReleaseClient(  );
+        }
+    }
+    return bOkay;
+}
+
+
+void onListeningTCPSocketEvent(TCPSocketEvent e)
+{
+    switch(e)
+    {
+    case TCPSOCKET_ACCEPT:
+        if(!prvbMBPortAcceptClient())
+        {
+#ifdef MB_TCP_DEBUG
+            vMBPortLog( MB_LOG_DEBUG, "MBTCP-ERROR", "Error with client connection! Droping it.\r\n" );
+#endif
+            ListeningSock.close();
+        }
+        break;
+
+    default:
+#ifdef MB_TCP_DEBUG
+        vMBPortLog( MB_LOG_DEBUG, "MBTCP-ERROR", "Unexpeted condition!.\r\n" );
+#endif
+        ListeningSock.close();
+        break;
+     };
+}
+
+void onConnectedTCPSocketEvent(TCPSocketEvent e)
+{
+    switch(e)
+    {
+    case TCPSOCKET_READABLE:
+        if(!prvMBTCPGetFrame())prvvMBPortReleaseClient();
+        break;
+    case TCPSOCKET_CONNECTED:
+    case TCPSOCKET_WRITEABLE:
+        break;
+
+    case TCPSOCKET_CONTIMEOUT:
+    case TCPSOCKET_CONRST:
+    case TCPSOCKET_CONABRT:
+    case TCPSOCKET_ERROR:
+    case TCPSOCKET_DISCONNECTED:
+        prvvMBPortReleaseClient();
+        break;
+    default:
+        break;
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,315 @@
+/*
+ * FreeModbus Libary: BARE Demo Application
+ * Copyright (C) 2006 Christian Walter <wolti@sil.at>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * File: $Id: demo.c,v 1.1 2006/08/22 21:35:13 wolti Exp $
+
+* modified from: Thanassis Mavrogeorgiadis 5-12-2011
+ */
+
+#include "mbed.h"
+
+/* ----------------------- Modbus includes ----------------------------------*/
+#include "mb.h"
+#include "mbport.h"
+#include "mbconfig.h"
+
+
+#if MB_TCP_ENABLED == 1
+#include "EthernetNetIf.h"
+EthernetNetIf eth;  
+#endif
+
+extern Serial pc;
+
+DigitalOut led1(LED1);
+
+DigitalIn ENET_LINK(P1_25);
+DigitalOut LedLINK(LED4);
+Ticker EtherPinMonitor;
+
+#if MB_TCP_ENABLED == 1
+void EtherPinMonitorFunc(void)
+{
+  LedLINK = !ENET_LINK; 
+}
+#endif
+
+/* ----------------------- Defines ------------------------------------------*/
+#define REG_INPUT_START 1001
+#define REG_INPUT_NREGS 4
+
+#define REG_HOLDING_START       2001
+#define REG_HOLDING_NREGS       130
+
+#define REG_COIL_START (3000+1)
+#define REG_COIL_NREGS 8
+#define REG_COIL_BYTES REG_COIL_NREGS/8
+
+#define REG_DISC_START (4000+1)
+#define REG_DISC_NREGS 8
+#define REG_DISC_BYTES REG_DISC_NREGS/8
+
+#define SLAVE_ID 0x0A
+
+/* ----------------------- Static variables ---------------------------------*/
+static USHORT   usRegInputStart = REG_INPUT_START;
+static USHORT   usRegInputBuf[REG_INPUT_NREGS];
+static USHORT   usRegHoldingStart = REG_HOLDING_START;
+static USHORT   usRegHoldingBuf[REG_HOLDING_NREGS]={0x0123,0x4567,0x89AB,0xCDEF,0xDEAD,0xBEEF,0xDEAD,0xBEEF,0xDEAD,0xBEEF};
+static USHORT    usRegCoilStart = REG_COIL_START;
+static UCHAR     usRegCoilBuf[REG_COIL_BYTES]={0xA5};
+static USHORT    usRegDiscStart = REG_DISC_START;
+static UCHAR     usRegDiscBuf[REG_DISC_BYTES]={0x5A};
+
+/* ----------------------- Start implementation -----------------------------*/
+
+
+int main() {
+  eMBErrorCode    eStatus;
+
+#if MB_TCP_ENABLED == 1
+  pc.baud(115200);
+  EtherPinMonitor.attach(&EtherPinMonitorFunc, 0.01);
+  printf("Setting up...\n");
+  EthernetErr ethErr = eth.setup();
+  if(ethErr)
+  {
+    printf("Error %d in setup.\n", ethErr);
+    return -1;
+  }
+  printf("Setup OK\n");
+  IpAddr ip = eth.getIp();
+  printf("mbed IP Address is %d.%d.%d.%d\r\n", ip[0], ip[1], ip[2], ip[3]);
+#endif
+   
+  Timer tm;
+  tm.start();
+
+#if MB_RTU_ENABLED == 1
+    eStatus = eMBInit( MB_RTU, SLAVE_ID, 0, 9600, MB_PAR_NONE );
+#endif
+#if MB_ASCII_ENABLED == 1
+    eStatus = eMBInit( MB_ASCII, SLAVE_ID, 0, 9600, MB_PAR_NONE );
+#endif
+#if MB_TCP_ENABLED == 1
+    eStatus = eMBTCPInit( MB_TCP_PORT_USE_DEFAULT );
+#endif
+    if (eStatus != MB_ENOERR )
+      printf( "can't initialize modbus stack!\r\n" );
+
+    /* Enable the Modbus Protocol Stack. */
+    eStatus = eMBEnable(  );
+    if (eStatus != MB_ENOERR )
+      fprintf( stderr, "can't enable modbus stack!\r\n" );
+
+  // Initialise some registers
+  usRegInputBuf[1] = 0x1234;
+  usRegInputBuf[2] = 0x5678;
+  usRegInputBuf[3] = 0x9abc;        
+
+  while(true)
+  {
+#if MB_TCP_ENABLED == 1
+    Net::poll();
+#endif
+/*
+    if(tm.read()>.5)
+    {
+      led1=!led1; //Show that we are alive
+      tm.start();
+    }
+*/
+    eStatus = eMBPoll(  );
+            
+    /* Here we simply count the number of poll cycles. */
+    usRegInputBuf[0]++;
+  }
+  //return 0;
+}
+
+eMBErrorCode
+eMBRegInputCB( UCHAR * pucRegBuffer, USHORT usAddress, USHORT usNRegs )
+{
+    eMBErrorCode    eStatus = MB_ENOERR;
+    int             iRegIndex;
+
+    if( ( usAddress >= REG_INPUT_START )
+        && ( usAddress + usNRegs <= REG_INPUT_START + REG_INPUT_NREGS ) )
+    {
+        iRegIndex = ( int )( usAddress - usRegInputStart );
+        while( usNRegs > 0 )
+        {
+            *pucRegBuffer++ = ( unsigned char )( usRegInputBuf[iRegIndex] >> 8 );
+            *pucRegBuffer++ = ( unsigned char )( usRegInputBuf[iRegIndex] & 0xFF );
+            iRegIndex++;
+            usNRegs--;
+        }
+    }
+    else
+    {
+        eStatus = MB_ENOREG;
+    }
+    return eStatus;
+}
+
+eMBErrorCode
+eMBRegHoldingCB( UCHAR * pucRegBuffer, USHORT usAddress, USHORT usNRegs, eMBRegisterMode eMode )
+{
+    eMBErrorCode    eStatus = MB_ENOERR;
+    int             iRegIndex;
+
+    if( ( usAddress >= REG_HOLDING_START ) &&
+        ( usAddress + usNRegs <= REG_HOLDING_START + REG_HOLDING_NREGS ) )
+    {
+        iRegIndex = ( int )( usAddress - usRegHoldingStart );
+        switch ( eMode )
+        {
+            /* Pass current register values to the protocol stack. */
+        case MB_REG_READ:
+            while( usNRegs > 0 )
+            {
+                *pucRegBuffer++ = ( UCHAR ) ( usRegHoldingBuf[iRegIndex] >> 8 );
+                *pucRegBuffer++ = ( UCHAR ) ( usRegHoldingBuf[iRegIndex] & 0xFF );
+                iRegIndex++;
+                usNRegs--;
+            }
+            break;
+
+            /* Update current register values with new values from the
+             * protocol stack. */
+        case MB_REG_WRITE:
+            while( usNRegs > 0 )
+            {
+                usRegHoldingBuf[iRegIndex] = *pucRegBuffer++ << 8;
+                usRegHoldingBuf[iRegIndex] |= *pucRegBuffer++;
+                iRegIndex++;
+                usNRegs--;
+            }
+        }
+    }
+    else
+    {
+        eStatus = MB_ENOREG;
+    }
+    return eStatus;
+}
+
+/* 
+ * Following implementation is not actually checked.
+ */
+
+eMBErrorCode
+eMBRegCoilsCB( UCHAR * pucRegBuffer, USHORT usAddress, USHORT usNCoils, eMBRegisterMode eMode )
+{
+    eMBErrorCode    eStatus = MB_ENOERR;
+    int             iIntRegIndex;
+    int                iIntBufNum;
+    int                iIntBitNum;
+    int                iExtRegIndex=0;
+    int                iExtBufNum;
+    int                iExtBitNum;
+    UCHAR            ucTemp;
+    if( ( usAddress >= REG_COIL_START )
+        && ( usAddress + usNCoils <= REG_COIL_START + REG_COIL_NREGS ) )
+    {
+        iIntRegIndex = ( int )( usAddress - usRegCoilStart );
+
+        while( usNCoils > 0 )
+        {
+            iIntBufNum=iIntRegIndex/8;
+            iIntBitNum=iIntRegIndex%8;
+            iExtBufNum=iExtRegIndex/8;
+            iExtBitNum=iExtRegIndex%8;
+
+            switch ( eMode )
+            {
+            case MB_REG_READ:
+                // Read coils
+                if(iExtBitNum==0){
+                    pucRegBuffer[iExtBufNum]=0;
+                }
+                ucTemp=(usRegCoilBuf[iIntBufNum]>>iIntBitNum) & 1;
+                pucRegBuffer[iExtBufNum]|=ucTemp<<iExtBitNum;
+                break;
+
+            case MB_REG_WRITE:
+                // Write coils
+                if(iIntBitNum==0){
+                    usRegCoilBuf[iIntBufNum]=0;
+                }
+                ucTemp=(pucRegBuffer[iExtBufNum]>>iExtBitNum) & 1;
+                usRegCoilBuf[iIntBufNum]|=ucTemp<<iIntBitNum;
+                break;
+            }
+            iIntRegIndex++;
+            iExtRegIndex++;
+            usNCoils--;
+
+        }
+    }
+    else
+    {
+        eStatus = MB_ENOREG;
+    }
+
+    return eStatus;
+}
+
+eMBErrorCode
+eMBRegDiscreteCB( UCHAR * pucRegBuffer, USHORT usAddress, USHORT usNDiscrete )
+{
+    eMBErrorCode    eStatus = MB_ENOERR;
+    int             iIntRegIndex;
+    int                iIntBufNum;
+    int                iIntBitNum;
+    int                iExtRegIndex=0;
+    int                iExtBufNum;
+    int                iExtBitNum;
+    UCHAR            ucTemp;
+    if( ( usAddress >= REG_DISC_START )
+        && ( usAddress + usNDiscrete <= REG_DISC_START + REG_DISC_NREGS ) )
+    {
+        iIntRegIndex = ( int )( usAddress - usRegDiscStart );
+
+        while( usNDiscrete > 0 )
+        {
+            iIntBufNum=iIntRegIndex/8;
+            iIntBitNum=iIntRegIndex%8;
+            iExtBufNum=iExtRegIndex/8;
+            iExtBitNum=iExtRegIndex%8;
+
+            // Read discrete inputs
+            if(iExtBitNum==0){
+                pucRegBuffer[iExtBufNum]=0;
+            }
+            ucTemp=(usRegDiscBuf[iIntBufNum]>>iIntBitNum) & 1;
+            pucRegBuffer[iExtBufNum]|=ucTemp<<iExtBitNum;
+
+            iIntRegIndex++;
+            iExtRegIndex++;
+            usNDiscrete--;
+
+        }
+    }
+    else
+    {
+        eStatus = MB_ENOREG;
+    }
+
+    return eStatus;
+}
